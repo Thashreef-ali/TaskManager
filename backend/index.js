0a1578b783e32d3774489c1f28559005214fdf37
thashreef <thashreefalit@gmail.com>
2025-05-30 22:26:13 +0530

    cors fixed

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,6 @@ const port = process.env.PORT || 4000;
 // Middlewares
 app.use(cors({
   origin: ['https://taskmanager-frontend-aeyy.onrender.com'],
-  credentials: true,
 }));
 app.use(express.json());
 
