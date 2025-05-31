0dbe3b69d8e6245f9ea6d5c39b253b56d70b1d46
thashreef <thashreefalit@gmail.com>
2025-05-31 08:55:21 +0530

    checking cors issue

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,9 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin:['https://taskmanager-frontend-aeyy.onrender.com']
+}));
 app.use(express.json());
 
 // Routes
